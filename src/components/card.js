7e045115a52511a2425d031dd70fcc3f3fad8793
Prashanth <udupaprashanthkrishna@gmail.com>
2023-01-28 08:42:19 +0530

    modified:   src/components/card.css 	modified:   src/components/card.js 	modified:   src/components/moviecards.css 	modified:   src/components/moviecards.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./card.css";
 function Card(props) {
-  const { id, title, genre, rating } = props;
+  const { id, title, genre1, genre2, rating, similar, showLike } = props;
   const [MovieDetails, SetMovieDetails] = useState([]);
+  // const [SimilarMovies, SetSimilarMovies] = useState([]);
+  const [isFavorite, SetIsFavorite] = useState(false);
   // const GetOtherMovies = async () => {
   //   var response = await fetch(`https://localhost:7023/api/Director/${id}`);
   //   if (response.ok) {
@@ -23,27 +27,68 @@ function Card(props) {
       console.log(`Error fetching movie ${id}`);
     }
   };
+  const FavoriteHandler = async () => {
+    SetIsFavorite(true);
+    var response = await fetch(
+      `https://localhost:7265/api/Movie/GetMovieByGenre?genre1=${genre1}&genre2=${genre2}`
+    );
+    var data = await response.json();
+    // console.log(data);
+    let movieArray = [];
+    let movieArray2 = [];
+    data.forEach((movie) => {
+      if (movie.movieId === id) {
+      } else if (
+        (movie.genre1 === genre1 && movie.genre2 === genre2) ||
+        (movie.genre1 === genre2 && movie.genre2 === genre1)
+      ) {
+        movieArray.push(movie);
+      } else {
+        movieArray2.push(movie);
+      }
+    });
+    movieArray2 = movieArray2.sort((a, b) => b.rating - a.rating);
+    movieArray = movieArray.sort((a, b) => b.rating - a.rating);
+    movieArray = movieArray.concat(movieArray2);
+    // SetSimilarMovies(movieArray);
+    similar(movieArray);
+    // console.log(movieArray);
+  };
 
   return (
-    <div className="moviecard" onClick={OnClickHandler}>
+    <div className="moviecard">
       <div className="movieimg">
+        {!isFavorite && showLike && (
+          <FavoriteBorderIcon
+            style={{ fill: "rgb(125, 44, 199)" }}
+            onClick={FavoriteHandler}
+          />
+        )}
+
+        {isFavorite && showLike && (
+          <FavoriteIcon style={{ fill: "rgb(125, 44, 199)" }} />
+        )}
         {/* <img src={require(`./../components/assets/${src}`)} alt="imageposter" /> */}
         {/* <img src={img} alt="imageposter" /> */}
       </div>
       <div className="movieinfo">
-        <h4>{title}</h4>
-        <small>{genre}</small>
+        <h4 onClick={OnClickHandler}>{title}</h4>
+        <small>
+          {genre1}, {genre2}
+        </small>
         <small>Rating : {rating}</small>
         {MovieDetails.length !== 0 && (
           <>
             <small>
-              Directed By : <span>{MovieDetails.directorId}</span>
+              <u>Director</u>
+              <br /> <span>{MovieDetails.directorName}</span>
             </small>
             <small>
-              Starring :
+              <u>Starring</u>
+              <br />
               {MovieDetails.actors.map((actor) => {
                 return (
-                  <span id={actor.actorId}>
+                  <span id={actor.actorId} key={actor.actorId}>
                     {actor.actorName}
                     <br />
                   </span>
--- src/components/moviecards.js
+++ src/components/moviecards.js
@@ -1,21 +1,48 @@
 import Card from "./card";
+import { useState } from "react";
 import "./moviecards.css";
 function MovieCards(props) {
   const { movies } = props;
+  const [SimilarMovies, SetSimilarMovies] = useState([]);
   return (
-    <div className="moviecards">
-      {movies.map((movie) => {
-        return (
-          <Card
-            key={movie.movieId}
-            id={movie.movieId}
-            title={movie.movieName}
-            genre={movie.genre1}
-            rating={movie.rating}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="similar">
+        {SimilarMovies.length !== 0 && <span>You may also like </span>}
+        <div className="similarMovies">
+          {SimilarMovies.length !== 0 &&
+            SimilarMovies.map((movie) => {
+              return (
+                <Card
+                  key={movie.movieId}
+                  id={movie.movieId}
+                  title={movie.movieName}
+                  genre1={movie.genre1}
+                  genre2={movie.genre2}
+                  rating={movie.rating}
+                  similar={SetSimilarMovies}
+                  showLike={false}
+                />
+              );
+            })}
+        </div>
+      </div>
+      <div className="moviecards">
+        {movies.map((movie) => {
+          return (
+            <Card
+              key={movie.movieId}
+              id={movie.movieId}
+              title={movie.movieName}
+              genre1={movie.genre1}
+              genre2={movie.genre2}
+              rating={movie.rating}
+              similar={SetSimilarMovies}
+              showLike={true}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
 export default MovieCards;
